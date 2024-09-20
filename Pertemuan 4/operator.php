c0d7daa8768cd63d93724a241ce76c0582490c2e
yassnothere <achmadiaz122@gmail.com>
2024-09-20 09:23:56 +0700

    Praktikum 3 Soal No. 3.2

--- Pertemuan 4/operator.php
+++ Pertemuan 4/operator.php
@@ -13,6 +13,22 @@ echo "Hasil Tambah = $a + $b = $hasilTambah<br>";
 echo "Hasil Kurang = $a - $b = $hasilKurang<br>";
 echo "Hasil Kali = $a * $b = $hasilKali<br>";
 echo "Hasil Bagi = $a / $b = $hasilBagi<br>";
-echo "Hasil sisaBagi = $a % $b = $sisaBagi<br>";
-echo "Hasil pangkat = $a ** $b = $pangkat<br>";
+echo "Hasil Sisa Bagi = $a % $b = $sisaBagi<br>";
+echo "Hasil Pangkat = $a ** $b = $pangkat<br>";
+
+echo "<br><br>";
+
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama = $a >= $b;
+
+echo "Apakah $a sama dengan $b? : " . ($hasilSama ? 'true' : 'false') . "<br>";
+echo "Apakah $a tidak sama dengan $b? : " . ($hasilTidakSama ? 'true' : 'false') . "<br>";
+echo "Apakah $a lebih kecil dari $b? : " . ($hasilLebihKecil ? 'true' : 'false') . "<br>";
+echo "Apakah $a lebih besar dari $b? : " . ($hasilLebihBesar ? 'true' : 'false') . "<br>";
+echo "Apakah $a lebih kecil atau sama dengan $b? : " . ($hasilLebihKecilSama ? 'true' : 'false') . "<br>";
+echo "Apakah $a lebih besar atau sama dengan $b? : " . ($hasilLebihBesarSama ? 'true' : 'false') . "<br>";
 ?>
