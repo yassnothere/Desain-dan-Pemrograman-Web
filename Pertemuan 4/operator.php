b7d423e0cdb4a5c39b3edd19b65a663a6ce21285
yassnothere <achmadiaz122@gmail.com>
2024-09-20 09:10:31 +0700

    Praktikum 3 Soal No. 3.1

---
+++ Pertemuan 4/operator.php
@@ -0,0 +1,18 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil Tambah = $a + $b = $hasilTambah<br>";
+echo "Hasil Kurang = $a - $b = $hasilKurang<br>";
+echo "Hasil Kali = $a * $b = $hasilKali<br>";
+echo "Hasil Bagi = $a / $b = $hasilBagi<br>";
+echo "Hasil sisaBagi = $a % $b = $sisaBagi<br>";
+echo "Hasil pangkat = $a ** $b = $pangkat<br>";
+?>
