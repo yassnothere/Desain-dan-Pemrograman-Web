3f8cf903ec92c9bdba08899751fbbe411db3b913
yassnothere <achmadiaz122@gmail.com>
2024-09-26 18:49:41 +0700

    Tugas Praktikum 4.8

--- Pertemuan 4/TugasPRaktikum4.8.php
+++ Pertemuan 4/TugasPRaktikum4.8.php
@@ -1,10 +1,9 @@
 <?php
 $totalSkor = 700; // anggap saja skor dari pemain game 650
-$isDapatHadiah = false;
 
-($totalSkor > 500) ? $isDapatHadiah = true : $isDapatHadiah = false;
+// Menggunakan operasi ternary untuk menentukan apakah pemain mendapatkan hadiah
+$isDapatHadiah = ($totalSkor > 500) ? true : false;
 
 echo "Total skor pemain adalah : $totalSkor <br>";
 echo "Apakah pemain mendapatkan hadiah tambah? (" . ($isDapatHadiah ? "Yes" : "No") . ")";
-
 ?>
