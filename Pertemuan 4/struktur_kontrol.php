93d195ba74e7a79f46cda7cf609b316ec66530c1
yassnothere <achmadiaz122@gmail.com>
2024-09-26 17:30:13 +0700

    Praktikum 4 Soal No 4.1

--- Pertemuan 4/struktur_kontrol.php
+++ Pertemuan 4/struktur_kontrol.php
@@ -1,2 +1,15 @@
 <?php
+$nilaiNumerik = 92;
+
+if ($nilaiNumerik >= 90 && $nilaiNumerik <= 100) {
+  echo "Nilai huruf: A";
+} elseif ($nilaiNumerik >= 80 && $nilaiNumerik < 90) {
+  echo "Nilai huruf: B";
+} elseif ($nilaiNumerik >= 70 && $nilaiNumerik < 80) {
+  echo "Nilai huruf: C";
+} elseif ($nilaiNumerik < 70) {
+  echo "Nilai huruf: D";
+}
+
+echo "<br>";
 ?>
