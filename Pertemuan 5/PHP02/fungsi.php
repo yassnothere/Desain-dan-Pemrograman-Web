094927ad88e20115fd09ac210cf01c43ef39a6e2
yassnothere <achmadiaz122@gmail.com>
2024-10-03 16:59:03 +0700

    Praktikum 1 Soal no1 PHP02

---
+++ Pertemuan 5/PHP02/fungsi.php
@@ -0,0 +1,17 @@
+<?php
+
+function perkenenalan($nama, $salam){
+    echo $salam.", ";
+    echo "Perkenalkan nama Saya ".$nama."<br/> ";
+    echo "Senang berkenalan dengan Anda<br/> ";
+}
+
+perkenenalan("Diaz, Hallo");
+
+echo"<hr>"
+
+$saya = "Diaz";
+$ucapanSalam = "Selamat Pagi"
+
+perkenenalan($saya, $ucapanSalam)
+?>
