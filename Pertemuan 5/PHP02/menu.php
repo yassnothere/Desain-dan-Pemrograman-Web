6c5108ffa708c289427b419fa2f71b6caa013cf9
yassnothere <achmadiaz122@gmail.com>
2024-10-03 17:56:35 +0700

    SOALNO 7

---
+++ Pertemuan 5/PHP02/menu.php
@@ -0,0 +1,31 @@
+<?php
+$menu = [
+    ["nama" => "Beranda"],
+    [
+      "nama" => "Berita",
+      "subMenu" => [
+        [
+          "nama" => "Wisata",
+          "subMenu" => [
+            ["nama" => "Pantai"],
+            ["nama" => "Gunung"],
+          ]
+        ],
+        ["nama" => "Kuliner"],
+        ["nama" => "Hiburan"],
+      ]
+    ],
+    ["nama" => "Tentang"],
+    ["nama" => "Kontak"],
+  ];
+
+function tampilkanMenuBertingkat (array $menu){
+    echo "<ul>";
+    foreach ($menu as $key => $item){
+        echo "<li>{$item['nama']}</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
+?>
