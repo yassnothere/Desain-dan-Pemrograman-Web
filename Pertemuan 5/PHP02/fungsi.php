c0e3f29b28e32532b972504b817ebfb1b9930048
yassnothere <achmadiaz122@gmail.com>
2024-10-03 17:12:00 +0700

    Praktikum 1 Soal no 4

--- Pertemuan 5/PHP02/fungsi.php
+++ Pertemuan 5/PHP02/fungsi.php
@@ -1,8 +1,16 @@
 <?php
 
+//membuat fungsi 
+function hitungUmur($thn_lahir, $thn_sekarang) { 
+  $umur = $thn_sekarang -  $thn_lahir;
+  return $umur;
+}
+
+
 function perkenenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan nama Saya ".$nama."<br/> ";
+    echo "Umur Saya adalah ". hitungUmur(2004, 2024) . " tahun <br/>";
     echo "Senang berkenalan dengan Anda<br/> ";
 }
 
