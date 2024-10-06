6b192d00db3673220c17a5b602e70d7065ea8def
yassnothere <achmadiaz122@gmail.com>
2024-10-06 11:26:33 +0700

    Praktikum 1 Soal No 1

--- Pertemuan 6/array_1.php
+++ Pertemuan 6/array_1.php
@@ -6,10 +6,9 @@
     <h2>Array Terindeks</h2>
 <?php
     $ListDosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
-
-    echo $ListDosen[2] . "<br>";
-    echo $ListDosen[0] . "<br>";
-    echo $ListDosen[1] . "<br>";
+    foreach ($ListDosen as $dosen) {
+        echo $dosen . "<br>";
+    }
 ?>
 </body>
 </html>
