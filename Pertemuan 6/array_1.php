152e07cb816831f3cbdb9a07b8c1cbb5f93d36d3
yassnothere <achmadiaz122@gmail.com>
2024-10-06 11:23:06 +0700

    Praktitkum 1 Soal No 1

---
+++ Pertemuan 6/array_1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <h2>Array Terindeks</h2>
+<?php
+    $ListDosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    echo $ListDosen[2] . "<br>";
+    echo $ListDosen[0] . "<br>";
+    echo $ListDosen[1] . "<br>";
+?>
+</body>
+</html>
