47d073ec8fb12f1163dd66dd3a622c7a966cef80
yassnothere <achmadiaz122@gmail.com>
2024-10-06 11:55:25 +0700

    Menggabungkan HTML dan PHP | Soal No 17

---
+++ Pertemuan 6/HTMLdanPHP.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Cara 01</title>
+</head>
+<body>
+  <p>Tanggal Hari ini : <?php echo date("d-m-Y");?></p>
+</body>
+</html>
---
+++ Pertemuan 6/HTMLdanPHPCara2.php
@@ -0,0 +1,8 @@
+<?php
+echo"<html>";
+echo"<head><title>Cara02</title></head>";
+echo"<body>";
+echo"<p>Tanggal Hari ini : ".date("d-m-Y") . "</p>";
+echo"</body>";
+echo"</html>";
+?>
