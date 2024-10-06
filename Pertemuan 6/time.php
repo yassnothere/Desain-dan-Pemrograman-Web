4692fe80be6e2162cc237c9d64721380a5006752
yassnothere <achmadiaz122@gmail.com>
2024-10-06 12:05:42 +0700

    Date and Time | Soal No 20

---
+++ Pertemuan 6/time.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+  <h3>Time</h3>
+  <?php
+    date_default_timezone_set("asia/jakarta");
+    echo date("h:i:sa");
+  ?>
+</body>
+</html>
