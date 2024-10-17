1c09d2a48ac96adf3f0c7e1019518875af6fcafb
yassnothere <achmadiaz122@gmail.com>
2024-10-17 19:20:31 +0700

    Praktikum 6 | Soal No. 6.2

---
+++ Pertemuan 7/form_ajax.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contoh Form dengan PHP dan jQuery</title>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+
+<form id="myForm" method="post">
+  <label for="buah">Pilih Buah:</label>
+  <select name="buah" id="buah">
+    <option value="apel">Apel</option>
+    <option value="pisang">Pisang</option>
+    <option value="mangga">Mangga</option>
+    <option value="jeruk">Jeruk</option>
+  </select>
+  <br>
+
+  <label>Pilih Warna Favorit:</label><br>
+  <input type="checkbox" name="warna[]" value="merah"> Merah<br>
+  <input type="checkbox" name="warna[]" value="biru"> Biru<br>
+  <input type="checkbox" name="warna[]" value="hijau"> Hijau<br>
+  <br>
+
+  <label>Pilih Jenis Kelamin:</label><br>
+  <input type="radio" name="jenis_kelamin" value="laki-laki"> Laki-laki<br>
+  <input type="radio" name="jenis_kelamin" value="perempuan"> Perempuan<br>
+  <br>
+
+  <input type="submit" value="Submit">
+</form>
+
+<div id="hasil"><!--Hasil akan ditampilkan di sini...--></div>
+
+<script>
+  $(document).ready(function() {
+    $("#myForm").submit(function(e) {
+      e.preventDefault(); // Mencegah pengiriman form secara default
+
+      // Mendapatkan data form
+      var formData = $(this).serialize();
+
+      // Kirim data ke server PHP
+      $.ajax({
+        url: "proses_lanjut.php", // Ganti dengan nama file PHP yang sesuai
+        type: "POST",
+        data: formData,
+        success: function(response) {
+          // Tampilkan hasil dari server di div "hasil"
+          $("#hasil").html(response);
+        }
+      });
+    });
+  });
+</script>
+
+</body>
+</html>
